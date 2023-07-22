2be0f5b58f499f4f028d3bdbd66a235a0e4c9d9c
trangiabao2702 <meikaruku@gmail.com>
2023-07-22 12:34:58 +0700

    first commit

---
+++ API_Server/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult getProfile()
+        {
+            string[] profile = new string[] { "A", "AB", "ABC" };
+
+            return Ok(profile);
+        }
+    }
+}
