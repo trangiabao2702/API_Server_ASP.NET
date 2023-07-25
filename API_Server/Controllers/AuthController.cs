f147a673df6669f8daf66dcfdb73e9030ce13d8d
trangiabao2702 <meikaruku@gmail.com>
2023-07-25 12:45:00 +0700

    add api auth/refresh-token

--- API_Server/Controllers/AuthController.cs
+++ API_Server/Controllers/AuthController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Linq;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
@@ -18,6 +19,7 @@ namespace API_Server.Controllers
     {
         private readonly DataContext _context;
         private readonly IConfiguration _configuration;
+        private static Dictionary<string, int> _refreshTokens = new Dictionary<string, int>();
 
         public AuthController(DataContext context, IConfiguration configuration) 
         {
@@ -26,11 +28,11 @@ namespace API_Server.Controllers
         }
 
         [NonAction]
-        private string CreateToken(User user, string tokenType = "Access")
+        private string CreateToken(int userId, string tokenType = "Access")
         {
             List<Claim> claims = new List<Claim>
             {
-                new Claim(ClaimTypes.Name, user.Username)
+                new Claim(ClaimTypes.Name, userId.ToString())
             };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("Jwt:" + tokenType + "Key").Value!));
@@ -104,8 +106,10 @@ namespace API_Server.Controllers
                     }
                     else
                     {
-                        string accessToken = CreateToken(user);
-                        string refreshToken = CreateToken(user, "Refresh");
+                        string accessToken = CreateToken(user.Id);
+                        string refreshToken = CreateToken(user.Id, "Refresh");
+
+                        _refreshTokens.Add(refreshToken, user.Id);
 
                         var response = new
                         {
@@ -120,6 +124,75 @@ namespace API_Server.Controllers
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("refresh-token")]
+        public ActionResult<Tokens> RefreshToken([FromBody] string refreshToken)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var validationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("Jwt:RefreshKey").Value!)),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+
+            try
+            {
+                // Validate Refresh Token
+                var principal = tokenHandler.ValidateToken(refreshToken, validationParameters, out var validatedToken);
+
+                if (validatedToken is JwtSecurityToken jwtSecurityToken)
+                {
+                    var checkHeader = jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha512Signature);
+
+                    if (!checkHeader)
+                    {
+                        return Unauthorized(); 
+                    }
+                }
+
+                // Check if Refresh Token was belong to the user who send request
+                int userId = int.Parse(principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value);
+                if (_refreshTokens.ContainsKey(refreshToken) && _refreshTokens.ContainsValue(userId))
+                {
+                    // Delete old Refresh Token and generate a new one
+                    _refreshTokens.Remove(refreshToken);
+                }
+                else
+                {
+                    return Forbid();
+                }
+
+                // Generate new Tokens
+                string newAccessToken = CreateToken(userId);
+                string newRefreshToken = CreateToken(userId, "Refresh");
+
+                // Add new Refresh Token to list
+                _refreshTokens.Add(newRefreshToken, userId);
+
+                // Response
+                var response = new
+                {
+                    accessToken = newAccessToken,
+                    refreshToken = newRefreshToken
+                };
+
+                return Ok(response);
+            }
+            catch (SecurityTokenExpiredException)
+            {
+                // Token expired
+                return Unauthorized();
+
+            }
+            catch (SecurityTokenException)
+            {
+                // Token is invalid
+                return Unauthorized();
+
             }
         }
     }
